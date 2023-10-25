538f9223468a2f7a857c571989f2b421098ffd3a
Daniel Palmer <114208935+dpalmer10@users.noreply.github.com>
2023-10-25 04:33:54 +0000

    initial edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'my-image':"url('https://cdn.discordapp.com/attachments/512873683509837826/1166582269146841118/692056.png?ex=654b0367&is=65388e67&hm=95c463629fa111d381a8029bf22209142b952de0965a039d791081d31a235a23&')"
+      }
+    },
   },
   plugins: [],
 }
